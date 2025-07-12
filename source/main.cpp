b0e9c6be36e3461f25bd628e3b17f8e70a02f9d4
drake <drakedrift@gmail.com>
2025-07-12 21:29:18 +0200

    chore: add experimental testing stuff in main entry

--- source/main.cpp
+++ source/main.cpp
@@ -27,9 +27,44 @@ SDK_EXPORT void RuntimeEntry(RegisterRuntime registerRuntime)
 
 #ifdef LUA_BUILD_EXE
 
+struct TestStruct
+{
+
+};
+
 int main(int argc, char** argv)
 {
-    
+
+    printf("main1\n");
+    lua::State test;
+    printf("main2\n");
+
+    test.GetGlobalNamespace();
+    {
+        test.BeginNamespace("yamp");
+        {
+            test.BeginClass("resource");
+            {
+                test.MemberVariable("name", +[](IResource* ptr){ return ptr->name; });
+                test.MemberVariable("resourcePath", +[](IResource* ptr){ return ptr->resourcePath; });
+                test.MemberVariable("resourceMainFile", +[](IResource* ptr){ return ptr->resourceMainFile; });
+            }
+            test.EndClass();
+
+            IResource resource;
+            resource.name = _strdup("Test resource name");
+            resource.resourcePath = _strdup("Test resource path");
+            resource.resourceMainFile = _strdup("Test resource main file");
+            printf("Test shit: %p\n", &resource);
+
+            test.PushObject(&resource, "resource");
+            test.RegisterVariable("thisResource");
+        }
+        test.EndNamespace();
+    }
+    test.EndNamespace();
+    test.RunFile("test.lua");
+
     return 0;
 }
 
